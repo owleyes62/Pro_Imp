b0fc8097973334748b973d57f3b454091c6f825f
Iwerson <109252776+owleyes62@users.noreply.github.com>
2025-03-18 10:58:54 +0000

    correção atividade04

--- Atividades/Atividade04.java
+++ Atividades/Atividade04.java
@@ -4,26 +4,26 @@ public class Atividade04 {
     public static Scanner input = new Scanner(System.in);
     
     public static void main(String[] args) {
-        int valorInicial, fatorIncremento, quantidadeTermos;
+        int v_inicial, fator_incremento, q_termos;
 
         System.out.println("Olá, bem-vindo ao gerador de Progressão Aritmética.");
         System.out.println("Por favor, preencha as informações abaixo:");
 
         System.out.print("Valor inicial: ");
-        valorInicial = input.nextInt();
+        v_inicial = input.nextInt();
 
         System.out.print("Fator de incremento: ");
-        fatorIncremento = input.nextInt();
+        fator_incremento = input.nextInt();
 
         System.out.print("Quantidade de termos: ");
-        quantidadeTermos = input.nextInt();
+        q_termos = input.nextInt();
 
         System.out.println("A Progressão Aritmética é:");
 
-        for (int i = 0; i < quantidadeTermos; i++) {
-            int termo = valorInicial + (i * fatorIncremento);
+        for (int i = 0; i < q_termos; i++) {
+            int termo = v_inicial + (i * fator_incremento);
             System.out.print(termo);
-            if (i < quantidadeTermos - 1) {
+            if (i < q_termos - 1) {
                 System.out.print(", ");
             }
         }
