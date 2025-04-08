96d7569d49742a75988901b299c71462149e464b
Iwerson <109252776+owleyes62@users.noreply.github.com>
2025-04-08 23:34:20 +0000

    AtividadeFrequencia&&ATUatv

---
+++ Atividades/atividadeCasa/Atividade03/tabelaFrequecia02.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class tabelaFrequecia02 {
+    public static final int tamV = 20;
+    public static final int valorV = 20;
+    public static Random gerador = new Random();
+    public static void main(String[] args){
+        int [] vetor = new int[tamV];
+    }
+    public static void tabelaFrequecias(int[] v){
+        for (int i = 0; i < v.length; i++) {
+            
+        }
+        
+    }
+    public static void exibir(int[] v2){
+        System.out.println("Valor | Frequência");
+        for (int i = 1; i < v2.length; i++) {
+            System.out.println("  " + i + "   |   " + v2[i]);
+        }
+    }
+}
